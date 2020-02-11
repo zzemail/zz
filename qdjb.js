5ca7ca9a0eb1d814a72bef702d69e6526f9bf675
zzemail <59478789+zzemail@users.noreply.github.com>
2020-02-12 01:28:24 +0800

    Update qdjb.js

--- qdjb.js
+++ qdjb.js
@@ -2,15 +2,15 @@
  * @supported 设备 ID
  */
 
-let Cookie = $prefs.valueForKey("ssrenCookie");
+let Cookie = $prefs.valueForKey("yhCookie");
 
 let Req = {
-  url: "https://xn--gmqz83awjh.org/auth/login",
+  url: "https://sakura.aoaomoe.club/auth/login",
   method: "POST",
   headers: {
     Cookie: Cookie,
-    Origin: "https://xn--gmqz83awjh.org",
-    Referer: "https://xn--gmqz83awjh.org/user",
+    Origin: "https://sakura.aoaomoe.club",
+    Referer: "https://sakura.aoaomoe.club/auth/login",
     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Mobile/15E148 Safari/604.1"
   }
 };
@@ -20,14 +20,14 @@ $task.fetch(Req).then(response => {
     let doc = JSON.parse(response.body);
     if (doc["ret"] == 1) {
       $notify(
-        "卡车极速",
+        "樱花穿随",
         "成功",
         `${doc["msg"]}\n已使用流量${doc["trafficInfo"]["lastUsedTraffic"]}\n剩余流量${doc["trafficInfo"]["unUsedTraffic"]}`
       );
     } else {
-      $notify("卡车极速", "成功", doc["msg"]);
+      $notify("樱花穿随", "成功", doc["msg"]);
     }
   } catch (error) {
-    $notify("卡车极速", "失败", error);
+    $notify("樱花穿随", "失败", error);
   }
 });
